39fe220f3dec0b9630fae0dd09d4ea22b53c04eb
Sheevam Sharma <45519621+sheevamsharma@users.noreply.github.com>
2021-01-09 14:24:14 -0800

    Text summarizer

--- summarizer.py
+++ summarizer.py
@@ -7,7 +7,9 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 
 
 # Text which will be inputted by the user to be summarized
-input_text = """" """"
+input_text = """ Master is an upcoming 2021 Indian Tamil-language action-thriller film co-written and directed by Lokesh Kanagaraj, and produced by Xavier Britto. The film stars Vijay and Vijay Sethupathi, with Malavika Mohanan, Arjun Das, Andrea Jeremiah and Shanthanu Bhagyaraj in supporting roles. The music is composed by Anirudh Ravichander, while cinematography and editing are performed by Sathyan Sooryan and Philomin Raj. Principal photography began in October 2019 and was completed in February 2020.
+
+Master was initially planned for theatrical release on 9 April 2020, but was postponed due to the COVID-19 pandemic in India. The makers of the film preferred to wait for a theatrical release, despite the digital streaming rights being sold to Prime Video. By the end of 2020, the film was confirmed for release on 13 January 2021 in theatres, during the week of Pongal. """
 
 
 # Text becomes tokenized
@@ -51,6 +53,6 @@ average = int(sumValues / len(sentenceValue))
 # Create summary
 summary =''
 for sentence in sentences:
-    if (sentence in sentenceValue) and sentenceValue[sentence] > (1.2 * average)):
+    if (sentence in sentenceValue) and sentenceValue[sentence] > (1.2 * average):
         summary += " " + sentence
 print(summary)
